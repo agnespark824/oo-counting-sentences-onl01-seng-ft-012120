870ad8652e08c9053b161f0f92023baddbaa63a7
Agnes Park <agnespark824@gmail.com>
2020-01-31 03:50:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    first_array = []
+    second_array = []
+    third_array = []
+    fourth_array = []
+    
+    first_array = self.split("!")
+    second_array << first_array
+    second_array.flatten!
 
+    second_array.each do |index|
+      third_array << index.split("?")
+    end
+    third_array.flatten!
+    
+    third_array.each do |index|
+      fourth_array << index.split(".")
+    end
+    
+    fourth_array.flatten!
+    fourth_array.size
   end
 end
