75fcf6a796257760a7409b2d83d3fcffe1e33466
Agnes Park <agnespark824@gmail.com>
2020-01-31 02:41:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = []
+    new_array << self.split("!").split("?").split(".")
+  binding.pry
+    new_array.flatten
   end
 end
