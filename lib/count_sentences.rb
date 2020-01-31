87c01ed59ccbe357c70973ecc483ebbb8bedaa77
Agnes Park <agnespark824@gmail.com>
2020-01-31 02:59:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,9 @@ class String
 
   def count_sentences
     first_array = []
-    second_array << self.split("!")
-    second_array.flatten each do {|index| index.split("?")}
+    second_array = self.split("!")
+    second_array.flatten each do |index| 
+      third_array = index.split("?")
     binding.pry
     new_array.flatten
   end
